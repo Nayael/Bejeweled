ecdea22ceb972df4ff8216de3f4af7db3ec4c387
Nicolas Vannier <nicolas.vannier93@gmail.com>
2012-11-25 15:01:45 +0100

    Finished > Site navigation

---
+++ js/navigation.js
@@ -0,0 +1,48 @@
+var Site = {};	// The Site namespace
+
+Site.initPage = function() {
+	var links = get('nav a');
+	for (var i = 0; i < links.length; i++) {
+		links[i].onclick = Site.onMenuClick;
+	};
+	Site.changePageFromHash();
+};
+
+/**
+ * Triggers on a click on a menu button
+ */
+Site.onMenuClick = function(event) {
+	// We set the clicked button as the current menu
+	var target = event.target || event.srcElement,
+		hash = window.location.hash;
+
+	if (target.getAttribute('href') == '#game')
+		window.location.hash = 'game';
+	else
+		window.location.hash = '';
+
+	if (window.location.hash != hash)
+		Site.changePageFromHash();
+};
+
+/**
+ * Changes the site page from the hash given in url
+ */
+Site.changePageFromHash = function () {
+	var links = get('nav a');
+
+	get("#site_content").style.display = window.location.hash === '' ? 'block' : 'none';
+	get("#game_content").style.display = window.location.hash === '' ? 'none' : 'block';
+	
+	for (var i = 0; i < links.length; i++) {
+		links[i].setAttribute('class', '');
+	};
+
+	if (window.location.hash === '')
+		links[0].setAttribute('class', 'current_menu');
+	else
+		get('#bt_' + window.location.hash.substr(1)).setAttribute('class', 'current_menu');
+};
+
+// We initialize the menu buttons listeners
+window.onload = Site.initPage();
